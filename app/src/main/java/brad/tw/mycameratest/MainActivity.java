59ee176ff850c120664dbb479141f7a3de4e5d92
Brad Chao <brad@brad.tw>
2016-09-22 14:20:37 +0800

    v2

--- app/src/main/java/brad/tw/mycameratest/MainActivity.java
+++ app/src/main/java/brad/tw/mycameratest/MainActivity.java
@@ -1,7 +1,11 @@
 package brad.tw.mycameratest;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void test1(View v){
+        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(it,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            afterTakePic1();
+        }else if (requestCode == RESULT_CANCELED){
+
+        }
+    }
+    private void afterTakePic1(){
+
+    }
+
+
+    public void test2(View v){
+
+    }
+
 }
